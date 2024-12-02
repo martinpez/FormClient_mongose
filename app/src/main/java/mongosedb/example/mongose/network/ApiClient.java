1e7ce2bb80043560ea3b593772df969ca7ae5564
Martin Elias <perezmercadomartin@gmail.com>
2024-12-01 19:27:28 -0500

    app finished

--- app/src/main/java/mongosedb/example/mongose/network/ApiClient.java
+++ app/src/main/java/mongosedb/example/mongose/network/ApiClient.java
@@ -4,10 +4,10 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
     // Trabajar localmente Emulador Android
-  // private static final String BASE_URL = "http://10.0.2.2:3000/api/";
+  private static final String BASE_URL = "http://10.0.2.2:3000/api/";
 
     //Para trabajar por medio de cualquiera otra red consultando su direccion ip el cumputador debe estar en la misma red
-    private static final String BASE_URL = "https://6da4-190-242-99-227.ngrok-free.app/api/";
+ //   private static final String BASE_URL = "https://6da4-190-242-99-227.ngrok-free.app/api/";
 
     private static Retrofit retrofit;
 
